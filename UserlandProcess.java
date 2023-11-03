a8b81d74a9160cbf3c3a2f826984621e1c0e9af7
Gregory Vincent Jr <gregvjrr@gmail.com>
2023-11-03 17:18:07 -0400

    Userland process

--- UserlandProcess.java
+++ UserlandProcess.java
@@ -2,4 +2,16 @@
  * @author Gregory Vincent
  */
 public abstract class UserlandProcess implements Runnable{
+
+    public byte Read(int address){
+        return 0;
+    }
+
+    public void Write(int address, byte vale){
+
+    }
+
+    private int findPageNumber(int address){
+        return 0;
+    }
 }
